7ef6a60189f8d821be3c2bb42ebd9d53b462d82d
Артём <DepthRel@gmail.com>
2020-04-08 21:21:32 +0300

    RelayCommand class and generic of this version added in ViewModels libs. Tests added for new class.

---
+++ Toolkit.Tests/ViewModels/RelayCommandTests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using Toolkit.ViewModels;
+
+namespace Toolkit.Tests.ViewModels
+{
+    [TestClass]
+    public class RelayCommandTests
+    {
+        [TestMethod]
+        public void InvokeCommandCorrect()
+        {
+            IList<string> list = new List<string>();
+
+            ICommand command = new RelayCommand(arg =>
+            {
+                list.Add((string)arg);
+            });
+
+            command.Execute("string");
+
+            Assert.AreEqual(list.Count, 1);
+            Assert.AreEqual(list.First(), "string");
+        }
+
+        [TestMethod]
+        public void InvokeGenericCommandCorrect()
+        {
+            IList<string> list = new List<string>();
+
+            ICommand command = new RelayCommand<string>(arg =>
+            {
+                list.Add(arg);
+            });
+
+            command.Execute("string");
+
+            Assert.AreEqual(list.Count, 1);
+            Assert.AreEqual(list.First(), "string");
+        }
+    }
+}
---
+++ Toolkit/ViewModels/RelayCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Toolkit.ViewModels
+{
+    /// <summary>
+    /// Class implementing ICommand interface for simplified implementation of commands
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged = delegate
+        {
+            // IT'S DOESN'T WORK FOR UWP PROJECT!!!
+
+            //add => CommandManager.RequerySuggested += value;
+            //remove => CommandManager.RequerySuggested -= value;
+        };
+
+        /// <summary>
+        /// The constructor of the command accepts the delegate of the execution method and the delegate of the condition for the execution of the command
+        /// </summary>
+        /// <param name="execute">One-argument execution method delegate</param>
+        /// <param name="canExecute">The delegate of the command execution condition that returns the Boolean value of the command execution ability</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+
+    /// <summary>
+    /// Generic class implementing ICommand interface for simplified implementation of commands
+    /// </summary>
+    /// <typeparam name="T">The type of object for which the command is executed</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+        private readonly Func<T, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged = delegate
+        {
+            // IT'S DOESN'T WORK FOR UWP PROJECT!!!
+
+            //add => CommandManager.RequerySuggested += value;
+            //remove => CommandManager.RequerySuggested -= value;
+        };
+
+        /// <summary>
+        /// The constructor of the command accepts the delegate of the execution method and the delegate of the condition for the execution of the command
+        /// </summary>
+        /// <param name="execute">One-argument execution method delegate</param>
+        /// <param name="canExecute">The delegate of the command execution condition that returns the Boolean value of the command execution ability</param>
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+    }
+}
