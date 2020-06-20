435254d2e0211f5fe482adfdd2e634dc2acc336c
Артём <DepthRel@gmail.com>
2020-06-20 23:59:07 +0300

    Added class "AppSettings" to store application settings. To access the settings, an indexer with a text key is used to identify the settings.

---
+++ Toolkit.Tests/AppSettingsTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toolkit.Tests
+{
+    [TestClass]
+    public class AppSettingsTests
+    {
+        [TestMethod]
+        public void WriteAndReadSettingCorrect()
+        {
+            string name = "Dev";
+            AppSettings.Setting["name"] = name;
+
+            Assert.AreEqual(name, AppSettings.Setting["name"]);
+        }
+
+        [TestMethod]
+        public void GetAllActualSettingsCorrect()
+        {
+            IEnumerable<string> settings = new List<string>() { "First", "Second", "Third" };
+
+            foreach (var setting in settings)
+            {
+                AppSettings.Setting[setting] = setting;
+            }
+
+            var actualSettings = AppSettings.Setting.AvailableSettings;
+            Assert.AreEqual(settings.Count(), actualSettings.Count());
+
+            foreach (var actualSetting in actualSettings)
+            {
+                bool isFind = false;
+
+                foreach (var setting in settings)
+                {
+                    if (setting == actualSetting)
+                    {
+                        isFind = true;
+                    }
+                }
+
+                if (!isFind)
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+    }
+}
---
+++ Toolkit/AppSettings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Toolkit
+{
+    public class AppSettings
+    {
+        #region Singleton implementation
+
+        private static readonly Lazy<AppSettings> instance;
+        public static AppSettings Setting => instance.Value;
+
+        static AppSettings()
+        {
+            instance = new Lazy<AppSettings>(() => new AppSettings());
+        }
+
+        #endregion
+
+        private readonly Dictionary<string, object> settings;
+
+        public object this[string key]
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(key) && settings.TryGetValue(key, out var setting))
+                {
+                    return setting;
+                }
+
+                throw new KeyNotFoundException($"Can't to find setting with name \"{key}\"");
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(key))
+                {
+                    throw new ArgumentException($"The setting name is in the wrong format");
+                }
+
+                settings.Add(key, value);
+            }
+        }
+
+        public IEnumerable<string> AvailableSettings => settings.Keys;
+
+        protected AppSettings()
+        {
+            settings = new Dictionary<string, object>();
+        }
+    }
+}
