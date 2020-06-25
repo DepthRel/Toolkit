20f3e9e5543884e564a060d444e3512685ea9987
Артём <DepthRel@gmail.com>
2020-06-26 00:45:05 +0300

    Added a new class representing the interval. It may be useful when comparing a range with some value, such as dates. For the new class, tests are implemented.

---
+++ src/Toolkit/Interval.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Toolkit.Contracts;
+
+namespace Toolkit
+{
+    /// <summary>
+    /// A generic class for representing an interval of two values. Provides methods for checking a value against a range.
+    /// </summary>
+    /// <typeparam name="T">Type descendant of class Object implementing interface IComparable and IComparable <></typeparam>
+    public class Interval<T> where T : IComparable, IComparable<T>, IEquatable<T>
+    {
+        protected readonly T left;
+        protected readonly T right;
+
+        /// <summary>
+        /// The first value  in the range. Necessarily less than the second value.
+        /// </summary>
+        public T Left => left;
+        /// <summary>
+        /// The value at the end of the range. Necessarily greater than the first value.
+        /// </summary>
+        public T Right => right;
+
+        /// <summary>
+        /// <para>
+        ///     Initializing an interval with two values. The constructor checks which of the values is greater.</para>
+        /// <para>
+        ///     If the order is not correct, the arguments are rearranged.
+        /// </para>
+        /// </summary>
+        /// <param name="first">The first value  in the range. Necessarily less than the second value.</param>
+        /// <param name="second">The value at the end of the range. Necessarily greater than the first value.</param>
+        /// <exception cref="ArgumentNullException">Thrown when one of parameters is null</exception>
+        public Interval(in T first, in T second)
+        {
+            Contract.NotNull<T, ArgumentNullException>(first);
+            Contract.NotNull<T, ArgumentNullException>(second);
+
+            if (first.CompareTo(second) < 0)
+            {
+                left = first;
+                right = second;
+            }
+            else
+            {
+                right = first;
+                left = second;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the value lies inside and on the border of the range
+        /// </summary>
+        /// <param name="value">Value to range ratio</param>
+        /// <returns><strong>The statement that the value lies within the range</strong></returns>
+        /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+        public bool Inside(in T value)
+        {
+            Contract.NotNull<T, ArgumentNullException>(value);
+
+            return left.CompareTo(value) <= 0 && right.CompareTo(value) >= 0;
+        }
+
+        /// <summary>
+        /// Checks if a value is out of range
+        /// </summary>
+        /// <param name="value">Value to range ratio</param>
+        /// <returns><strong>The statement that the value is not within the range</strong></returns>
+        /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+        public bool Outside(in T value)
+        {
+            Contract.NotNull<T, ArgumentNullException>(value);
+
+            return left.CompareTo(value) > 0 || right.CompareTo(value) < 0;
+        }
+
+        /// <summary>
+        /// Checks if the value is within the range excluding the border
+        /// </summary>
+        /// <param name="value">Value to range ratio</param>
+        /// <returns><strong>The statement that the value lies within the range not including the boundary</strong></returns>
+        /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+        public bool Between(in T value)
+        {
+            Contract.NotNull<T, ArgumentNullException>(value);
+
+            return left.CompareTo(value) < 0 && right.CompareTo(value) > 0;
+        }
+
+        /// <summary>
+        /// Checks if the value is after the interval
+        /// </summary>
+        /// <param name="value">Value to range ratio</param>
+        /// <returns><strong>The statement that the value is after the range</strong></returns>
+        /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+        public bool Beyond(in T value)
+        {
+            Contract.NotNull<T, ArgumentNullException>(value);
+
+            return right.CompareTo(value) < 0;
+        }
+
+        /// <summary>
+        /// Checks if the value is before the interval
+        /// </summary>
+        /// <param name="value">Value to range ratio</param>
+        /// <returns><strong>The statement that the value is before the range</strong></returns>
+        /// <exception cref="ArgumentNullException">Thrown when value is null</exception>
+        public bool Before(in T value)
+        {
+            Contract.NotNull<T, ArgumentNullException>(value);
+
+            return left.CompareTo(value) > 0;
+        }
+
+        #region Overrides
+
+        /// <summary>
+        /// Verifies the values of the transmitted interval
+        /// </summary>
+        /// <param name="obj">Interval of <see cref="Interval{T}"/> type</param>
+        /// <returns><strong>True if each value of the transmitted interval matches the current</strong></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is Interval<T> value)
+            {
+                if (Left.CompareTo(value.Left) == 0 && Right.CompareTo(value.Right) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Getting a unique hash code
+        /// </summary>
+        /// <returns><strong>Unique hash code</strong></returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var result = 0;
+                result = (result * 397) ^ Left.GetHashCode();
+                result = (result * 397) ^ Right.GetHashCode();
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Getting a string in the format: <example><code>{Left}..{Right}</code></example>
+        /// </summary>
+        /// <returns><strong>String like: 1..9</strong></returns>
+        public override string ToString()
+        {
+            return $"{Left}..{Right}";
+        }
+
+        #endregion
+
+        #region Type conversion operators
+
+        /// <summary>
+        /// Check for equality of two intervals
+        /// </summary>
+        /// <param name="left">First interval</param>
+        /// <param name="right">Second interval</param>
+        /// <returns><strong>True if each interval value is equal to each other</strong></returns>
+        public static bool operator ==(Interval<T> left, Interval<T> right) => left.Left.CompareTo(right.Left) == 0 && left.Right.CompareTo(right.Right) == 0;
+
+        /// <summary>
+        /// Check for inequality of two intervals
+        /// </summary>
+        /// <param name="left">First interval</param>
+        /// <param name="right">Second interval</param>
+        /// <returns><strong>True if each interval value is not equal to each other</strong></returns>
+        public static bool operator !=(Interval<T> left, Interval<T> right) => left.Left.CompareTo(right.Left) != 0 || left.Right.CompareTo(right.Right) != 0;
+
+        #endregion
+    }
+}
---
+++ tests/Toolkit.Tests/IntervalTests.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toolkit.Tests
+{
+    [TestClass]
+    public class IntervalTests
+    {
+        [TestMethod]
+        public void InitializeCorrect()
+        {
+            var interval = new Interval<int>(10, 1);
+
+            Assert.AreEqual(1, interval.Left);
+            Assert.AreEqual(10, interval.Right);
+        }
+
+        [TestMethod]
+        public void InsideCorrect()
+        {
+            var interval = new Interval<int>(1, 10);
+            bool result;
+
+            result = interval.Inside(-1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Inside(1);
+            Assert.AreEqual(true, result);
+
+            result = interval.Inside(5);
+            Assert.AreEqual(true, result);
+
+            result = interval.Inside(10);
+            Assert.AreEqual(true, result);
+
+            result = interval.Inside(12);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void BetweenCorrect()
+        {
+            var interval = new Interval<int>(1, 10);
+            bool result;
+
+            result = interval.Between(-1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Between(1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Between(5);
+            Assert.AreEqual(true, result);
+
+            result = interval.Between(10);
+            Assert.AreEqual(false, result);
+
+            result = interval.Between(12);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void OutsideCorrect()
+        {
+            var interval = new Interval<int>(1, 10);
+            bool result;
+
+            result = interval.Outside(-1);
+            Assert.AreEqual(true, result);
+
+            result = interval.Outside(1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Outside(5);
+            Assert.AreEqual(false, result);
+
+            result = interval.Outside(10);
+            Assert.AreEqual(false, result);
+
+            result = interval.Outside(12);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void BeyondCorrect()
+        {
+            var interval = new Interval<int>(1, 10);
+            bool result;
+
+            result = interval.Beyond(-1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Beyond(1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Beyond(5);
+            Assert.AreEqual(false, result);
+
+            result = interval.Beyond(10);
+            Assert.AreEqual(false, result);
+
+            result = interval.Beyond(12);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void BeforeCorrect()
+        {
+            var interval = new Interval<int>(1, 10);
+            bool result;
+
+            result = interval.Before(-1);
+            Assert.AreEqual(true, result);
+
+            result = interval.Before(1);
+            Assert.AreEqual(false, result);
+
+            result = interval.Before(5);
+            Assert.AreEqual(false, result);
+
+            result = interval.Before(10);
+            Assert.AreEqual(false, result);
+
+            result = interval.Before(12);
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void EqualsCorrect()
+        {
+            var firstInterval = new Interval<int>(1, 10);
+            var secondInterval = new Interval<int>(1, 10);
+
+            Assert.AreEqual(true, firstInterval.Equals(secondInterval));
+            Assert.AreEqual(true, firstInterval == secondInterval);
+            Assert.AreEqual(false, firstInterval != secondInterval);
+        }
+
+        [TestMethod]
+        public void NotEqualsCorrect()
+        {
+            var firstInterval = new Interval<int>(1, 10);
+            var secondInterval = new Interval<int>(-1, 10);
+
+            Assert.AreEqual(false, firstInterval.Equals(secondInterval));
+            Assert.AreEqual(false, firstInterval == secondInterval);
+            Assert.AreEqual(true, firstInterval != secondInterval);
+        }
+
+        [TestMethod]
+        public void EqualsHashCodeCorrect()
+        {
+            var firstInterval = new Interval<int>(1, 10);
+            var secondInterval = new Interval<int>(1, 10);
+
+            Assert.AreEqual(true, firstInterval.GetHashCode() == secondInterval.GetHashCode());
+        }
+
+        [TestMethod]
+        public void NotEqualsHashCodeCorrect()
+        {
+            var firstInterval = new Interval<int>(1, 10);
+            var secondInterval = new Interval<int>(-1, 10);
+
+            Assert.AreEqual(false, firstInterval.GetHashCode() == secondInterval.GetHashCode());
+        }
+    }
+}
