9c9defd68dd6465f051a3ed149b8260cbcb87cfe
Артём <DepthRel@gmail.com>
2020-08-10 20:24:38 +0300

    Reworking the IMessage interface and replacing IResolutionMessage with IRequest.

--- src/Toolkit.Components/UI/IMessage.cs
+++ src/Toolkit.Components/UI/IMessage.cs
@@ -3,15 +3,13 @@
 namespace Toolkit.Components.UI
 {
     /// <summary>
-    /// Interface for encapsulating a dialog box with a user, without interference with the UI
+    /// Interface for encapsulating a dialog service with a user, without interference with the UI
     /// </summary>
     public interface IMessage
     {
         /// <summary>
         /// Calling a dialogue for displaying messages
         /// </summary>
-        /// <param name="message">Message text</param>
-        /// <param name="title">The headline of the message (optional)</param>
-        void Write(string message, string title = null);
+        void Report();
     }
 }
--- src/Toolkit.Components/UI/IRequest.cs
+++ src/Toolkit.Components/UI/IRequest.cs
@@ -6,14 +6,12 @@ namespace Toolkit.Components.UI
     /// Interface for requesting a solution
     /// </summary>
     /// <typeparam name="T">Query result type</typeparam>
-    public interface IResolutionMessage<T>
+    public interface IRequest<T>
     {
         /// <summary>
         /// Calling a dialogue for decision making
         /// </summary>
-        /// <param name="message">Message text</param>
-        /// <param name="title">The headline of the message (optional)</param>
-        /// <returns><strong>User decision result</strong></returns>
-        T Write(string message, string title = null);
+        /// <returns><strong>Decision result</strong></returns>
+        T Request();
     }
 }
